f3afa832dbc3836d1ec0446bf5eed81bdc5a12eb
audioxengine <audioxengine@gmail.com>
2018-08-06 20:46:09 +0800

    Update

---
+++ js/index.js
@@ -0,0 +1,17 @@
+//JS for Version Box
+function versionVsbltyT() {
+  var x = document.getElementById("version-info");
+  if (x.style.visibility === "visible") {
+    x.style.visibility = "hidden";
+  } else {
+    x.style.visibility = "visible";
+  }
+}
+function versionVsbltyH() {
+  var x = document.getElementById("version-info");
+  if (x.style.visibility === "") {
+    x.style.visibility = "";
+  } else {
+    x.style.visibility = "";
+  }
+}
