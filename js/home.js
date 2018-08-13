4de3b4677cb7839fcfbe56348f5dcf0a4be8ebb4
audioxengine <audioxengine@gmail.com>
2018-08-14 02:27:55 +0800

    Update

--- js/home.js
+++ js/home.js
@@ -2,23 +2,37 @@
 
 function lqButtonToggle() {
     var lqContainer = document.getElementById("lq-container");
+    var lqAlbumIcon = document.getElementById("lq-cover-icon");
+    var hqContainer = document.getElementById("hq-container");
+
     lqContainer.classList.toggle("toggle-filter");
+    lqAlbumIcon.classList.toggle("active");
+    hqContainer.classList.toggle("inactive");
     $(".lq-toggle-play").toggle();
     $("#fetch-info").fadeToggle("500");}
   
 function hqButtonToggle() {
     var hqPlayButton = document.getElementById("hq-play-button");
     var lqContainer = document.getElementById("lq-container");
+    var hqContainer = document.getElementById("hq-container");
   
     lqContainer.classList.remove("toggle-filter");
     hqPlayButton.classList.toggle("boosted");
+    hqContainer.classList.remove("inactive");
     $(".hq-toggle-play").toggle();
     $("#fetch-info").fadeIn("500");}
   
   
 function hqButtonToggle1() {
     if( $('#lq-container').css('opacity') == '1' ) {
-      $(".lq-toggle-play").toggle();
+    $(".lq-toggle-play").toggle();  
+  }
+}
+function hqButtonToggle2() {
+    if( $('#hq-container').css('opacity') == '1' ) {
+        
+    var lqAlbumIcon = document.getElementById("lq-cover-icon");
+    lqAlbumIcon.classList.remove("active");
    }
   }
 function lqButtonToggle1() {
