5c8a0867efb2cdffe2d22e1c2d342da6dd04c054
audioxengine <audioxengine@gmail.com>
2018-08-15 06:48:54 +0800

    Update

--- js/index.js
+++ js/index.js
@@ -1,17 +1,17 @@
-//JS for Version Box
-function versionVsbltyT() {
-  var versionInfo = document.getElementById("version-info");
-  if (versionInfo.style.visibility === "visible") {
-    versionInfo.style.visibility = "hidden";
-  } else {
-    versionInfo.style.visibility = "visible";
-  }
-}
-function versionVsbltyH() {
-  var versionInfo = document.getElementById("version-info");
-  if (versionInfo.style.visibility === "") {
-    versionInfo.style.visibility = "";
-  } else {
-    versionInfo.style.visibility = "";
-  }
-}
+//JS for Version BOx
+function versionVsbltyT(){var versionInfo=document.getElementById("version-info");"visible"===versionInfo.style.visibility?versionInfo.style.visibility="hidden":versionInfo.style.visibility="visible"}function versionVsbltyH(){var versionInfo=document.getElementById("version-info");versionInfo.style.visibility,i.style.visibility=""}
+
+function versionUpdate(){
+  document.getElementById("version-update-value").textContent="Update Version 1.1";
+  document.getElementById("update-span-1").textContent="Welcome! As you can see, TrackBoostR is still under development. Stay tuned as we grow and feel free to reach me using the Contact Tab. Cheers!";
+
+  document.getElementById("update-span-2").textContent=" Latest Updates:";
+  document.getElementById("update-span-3").textContent="-- Added BoostD Track of The Month --";
+  document.getElementById("update-span-4").textContent="";
+  document.getElementById("update-span-5").textContent="";
+  document.getElementById("update-span-6").textContent="";
+  document.getElementById("update-span-7").textContent="";
+  document.getElementById("update-span-8").textContent="";
+  document.getElementById("update-span-9").textContent="";
+  document.getElementById("update-span-10").textContent="";
+};
