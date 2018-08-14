9a744c59594cf992ba954ceb250fdc24ea3d7323
audioxengine <audioxengine@gmail.com>
2018-08-15 06:52:15 +0800

    Update

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ function versionUpdate(){
   document.getElementById("version-update-value").textContent="Update Version 1.1";
   document.getElementById("update-span-1").textContent="Welcome! As you can see, TrackBoostR is still under development. Stay tuned as we grow and feel free to reach me using the Contact Tab. Cheers!";
 
-  document.getElementById("update-span-2").textContent=" Latest Updates:";
+  document.getElementById("update-span-2").textContent=" Latest Update(s):";
   document.getElementById("update-span-3").textContent="-- Added BoostD Track of The Month --";
   document.getElementById("update-span-4").textContent="";
   document.getElementById("update-span-5").textContent="";
